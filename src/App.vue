640e879c2fb8de8a4fb2cd39979bcfd635bb0183
github <Miura2110>
2021-12-07 13:53:04 +0900

    bootstrap

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,12 @@
       </tr>
       <div class="arrow"></div>
     </table>
+    <h1>This is an about page</h1>
+    <b-button
+      size="lg"
+      variant="outline-primary">
+      Bootstrap Button
+    </b-button>
   </div>
 </template>
 
