051cddd80d80c2515543d1af23f129f72dc494ad
TanakaMotoki86p <tanaka.motoki.86p@jp.nssol.nipponsteel.com>
2021-12-07 14:41:59 +0900

    スタイル調整

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <h2>{{ roadmap.name }}</h2>
-    <table>
+    <table align="center">
       <div v-for="(book, index) in roadmap.books" :key="book.title">
-        <td v-if="index!==0"> ⇓</td>
+        <tr>
+          <td v-if="index !== 0">⇓</td>
+        </tr>
         <tr>
           <td>
-            <img :src="book.cover_image" width="100px" height="100px"/>
+            <img :src="book.cover_image" width="100px" height="130px" />
           </td>
-          <td style="vertical-align: top;">
-           <table>
+          <td style="vertical-align: top">
+            <table style="text-align: left">
               <tr>
                 <td>タイトル:</td>
                 <td>{{ book.title }}</td>
@@ -47,7 +49,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
