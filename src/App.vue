29d5d735d53246e6059f65746f7acd1b8e2623a8
yuya nakagawa <nakagawa.yuya.p55@jp.nssol.nipponsteel.com>
2021-12-08 23:17:38 +0900

    QA要素を追加

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <div align="left">
-      <h2 class="page-header">{{ roadmap.name }}</h2>
-      <p class="lead" style="overflow-wrap:normal; text-algin=left">{{ roadmap.outline }}</p>
+    <div>
+      <h3 class="page-header" text-align="left">{{ roadmap.name }}</h3>
+      <p style="overflow-wrap:normal; algin=left; text-algin=left; padding-top:1%; padding-bottom:2%">{{ roadmap.outline }}</p>
     </div>    
-    <table align="center" style="padding-top:20px;">
+    <table align="center">
       <div v-for="(book, index) in roadmap.books" :key="book.title">
         <tr v-if="index!==0">
           <td align="center">
-            <i class="bi bi-chevron-double-down" style="font-size:3rem; color:darkcyan; padding-top: 10px"></i>
+            <i class="bi bi-chevron-double-down" style="font-size:3rem; color:darkcyan; padding-top:2%"></i>
           </td>
-          <td style="vertical-align:top; text-align:left; color:darkcyan">
-            <h4> {{ book.arrow_comment }}</h4>
+          <td style="vertical-align:middle; text-align:left; color:darkcyan">
+            <h7> {{ book.arrow_comment }}</h7>
           </td>
         </tr>
         <tr>
@@ -19,7 +19,7 @@
             <img :src="book.cover_image" width="100px" height="130px" />
           </td>
           <td style="vertical-align: top; padding-left: 30px">
-            <h3 style="margin:0px; overflow-wrap:normal">{{ book.title }}</h3>
+            <h5 style="margin:0px; overflow-wrap:normal">{{ book.title }}</h5>
             <table style="text-align: left">
               <tr>
                 <td>著者名:</td>
@@ -34,13 +34,20 @@
         </tr>
       </div>
     </table>
+
+    <QAview />
+
   </div>
 </template>
 
 <script>
+import QAview from "./components/QA.vue"
 import roadmap from "./assets/roadmaps/roadmap_schema.json";
 export default {
   name: "App",
+  components:{ 
+    QAview
+  },
   data() {
     return {
       roadmap: roadmap,
@@ -54,11 +61,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
-  margin-left: 250px;
-  margin-right: 250px;
-  margin-bottom: 60px;
+  margin-top: 3%;
+  margin-left: 20%;
+  margin-right: 20%;
+  margin-bottom: 3%;
 }
 </style>
---
+++ src/components/QA.vue
@@ -0,0 +1,58 @@
+<template> 
+<div style="padding-top:10%;">
+
+    <form>
+        <div v-for="question in QA.question" :key="question.question_name" id="QABOX">
+            <p style="overflow-wrap:normal; algin=left; text-algin=left;">
+                {{ question.question_name }}
+            </p>
+            <span>
+                <p v-for="answer in question.answer" :key="answer" 
+                    style="overflow-wrap:normal; algin=left; text-algin=left; padding-left:5%; padding-top:0.5%;">
+                    <i class="bi bi-arrow-return-right" style="font-size:1.2rem; color:darkcyan; padding-right:1%"/>
+                    {{ answer }}
+                </p>
+            </span>            
+            <div class="mb-3" style="padding-left:5%; padding-top:0.5%;">
+                <label for="Quastion" class="form-label">回答・返信</label>
+                <textarea class="form-control" id="QuastionDetail" rows="3"></textarea>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end" style="padding-top:1%;">
+                    <button type="answer" class="btn btn-success btn-sm">回答</button>
+                </div>
+            </div>
+        </div>
+        <div class="mb-3" style="padding-top:30px;">
+            <label for="Quastion" class="form-label">質問</label>
+                <textarea class="form-control" id="QuastionDetail" rows="3"></textarea>
+            <div id="emailHelp" class="form-text">ここに質問内容を記載してください</div>
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="anyone-check">匿名で質問を投稿</label>
+        </div>
+        <button type="quastion" class="btn btn-success">投稿</button>
+    </form>
+</div>
+   
+</template>
+    
+<script>
+import QA from "../assets/roadmaps/QA.json";
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      QA: QA,
+    };
+  }
+}
+</script>
+
+<style>
+#QABOX {
+  border-left: solid 5px #5d627b;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.22);
+  margin: 2%;
+  padding: 2% 
+}
+</style>
