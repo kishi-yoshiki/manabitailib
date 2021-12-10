8f923235ca5dc1eccc68ea7e2fd0c8f30e35b9f5
yuya nakagawa <nakagawa.yuya.p55@jp.nssol.nipponsteel.com>
2021-12-10 16:40:11 +0900

    ロードマップのコンポーネント化

--- src/components/Roadmap.vue
+++ src/components/Roadmap.vue
@@ -1,53 +1,35 @@
 <template>
-    <div>
-        <div align="left" style="padding-bottom: 2%">
-        <h3 class="page-header" style="font-weight:bold">{{ roadmap.name }}</h3>
-        <h5 style="margin-bottom: 15px;">作成者: {{ roadmap.creator.name }} </h5>
-        <p style="overflow-wrap:normal; text-algin=left">
-        {{ roadmap.outline }}
-        </p>
-        </div>
-        <div style="margin-bottom:5%;">
-        <table align="center">
-        <div v-for="(book, index) in roadmap.books" :key="book.title">
-            <tr v-if="index !== 0">
-            <td align="center">
-                <i
-                class="bi bi-chevron-double-down"
-                style="font-size: 3rem; color: darkcyan; padding-top: 10px"
-                ></i>
-            </td>
-            <td style="vertical-align: middle; text-align: left; color: darkcyan">
-                <h6>{{ book.arrow_comment }}</h6>
-            </td>
-            </tr>
-            <tr>
-            <td>
-                <img :src="book.cover_image" width="100px" height="130px" />
-            </td>
-            <td style="vertical-align:top; padding-left:30px">
-                <h5 style="margin: 0px; overflow-wrap: normal; font-weight: bold">{{ book.title }}</h5>
-                <table style="text-align: left">
-                <tr>
-                    <td>著者名:</td>
-                    <td>{{ book.author }}</td>
-                </tr>
-                <tr>
-                    <td>出版社:</td>
-                    <td>{{ book.publisher }}</td>
-                </tr>
-                </table>
-            </td>
-            </tr>
-        </div>
-        </table>
-        </div>    
-    </div>
+  <div id="app">
+    <Roadmap :roadmap="roadmap"></Roadmap>
+    <hr>
+    <roadmap-creator-info :creator="roadmap.creator"></roadmap-creator-info>
+    <hr>
+    <QAview />
+  </div>
 </template>
 
 <script>
+import Roadmap from './RoadmapDetail.vue';
+import RoadmapCreatorInfo from './RoadmapCreatorInfo.vue';
+import QAview from "./QA.vue"
+
 export default {
-  props: ["roadmap"],
+  name: "App",
+  components:{ 
+    Roadmap,
+    QAview,
+    RoadmapCreatorInfo
+  },
+  data() {
+    return {
+      roadmapId: "roadmap_schema", // 表示したロードマップのID。ロードマップの情報はassets/roadmapsフォルダに「(ロードマップID).json」というファイル名で配置されている。
+    };
+  },
+  computed: {
+    roadmap() {
+      return require("../assets/roadmaps/" + this.roadmapId + ".json");
+    },
+  }
 };
 </script>
 
--- src/router.js
+++ src/router.js
@@ -1,15 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 //import App from './App.vue'
-import Home from './views/Home.vue';
+import Roadmap from './components/Roadmap.vue';
 import Users from './views/Users.vue';
+//import Home from './views/Home.vue';
 
 Vue.use(Router);
 
 export default new Router({
     mode: "history",
     routes: [
-      {path: "/", component: Home},
+      {path: "/", component: Roadmap},
       {path: "/users/:id", component: Users, props: true}
     //    {path: "/roadmap/:id", component: App}
   ]
