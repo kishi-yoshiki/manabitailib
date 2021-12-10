c602bbfa4ae294491c30d077fffdd952bdc79ded
yuya nakagawa <nakagawa.yuya.p55@jp.nssol.nipponsteel.com>
2021-12-10 15:34:36 +0900

    router追加

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
@@ -7,6 +8,7 @@ Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
-new Vue({
+new Vue({  
+  router: router,
   render: h => h(App),
 }).$mount('#app')
