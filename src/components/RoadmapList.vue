f352e331df48cbcdd78e5197d8de10467384ece3
TanakaMotoki86p <tanaka.motoki.86p@jp.nssol.nipponsteel.com>
2021-12-13 14:44:05 +0900

    質問一覧表示

--- src/components/RoadmapList.vue
+++ src/components/RoadmapList.vue
@@ -11,17 +11,43 @@
         </b-tooltip>
       </router-link>
     </div>
+    <br>
+
+    <div v-for="question in questionList" :key="question.id">
+      {{question.question}}<br>
+    </div>
+    
   </div>
+
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      //質問リスト
+      questionList:[],
+    };
+  },
   computed: {
     roadmapEntries() {
       return require('../assets/roadmaps/roadmap_list.json');
     }
+  },
+  // jsonseverからデータを読み込んでquestionList変数に格納
+  created() {
+    this.read();
+  },
+  methods: {
+    // 質問一覧描画
+    read() {
+      console.log(this.QAid)
+      fetch("http://localhost:3000/thread")
+        .then((res) => res.json())
+        .then((res) => this.questionList = res)
+    },
   }
 }
 </script>
-<style scoped>
-</style>
+
+<style scoped></style>
